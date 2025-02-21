f941cbc1533769ef36f44659fd1e4ce528659fb5
dewo1357 <dewo135789@gmail.com>
2025-02-21 16:44:44 +0700

    Update Message Function Regist Socket v3

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
         API_URL : 'http://localhost:5000/'
     },
     production : {
-        API_URL : 'https://cruel-davita-sadeshop-79e55b22.koyeb.app/'
+        API_URL : 'https://cruel-davita-sadeshop-79e55b22.koyeb.app'
     }
 }
 
